bfe7a759e91039d75cabe7ce074d083eda36c480
RomanAlekseev <r.alekseev@andersenlab.com>
2019-09-25 17:09:30 +0300

    remove usless file from react-create-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <ul>
+            <div>
+              <a href="#1">Здесь будет Логотип</a>
+            </div>
+            <li>
+              <a href="#2">Домой</a>
+            </li>
+            <li>
+              <a href="#3">Обо Мне</a>
+            </li>
+            <li>
+              <a href="#4">Услуги</a>
+            </li>
+            <li>
+              <a href="#5">Связаться со Мной</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <section>
+          <h1>Сопровождение Джунов</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro unde
+            dolore illum expedita voluptate suscipit cupiditate dolorum,
+            corporis nesciunt fuga optio illo hic, deserunt necessitatibus omnis
+            nihil ipsa quaerat autem quos sint iste totam id. Fuga eos saepe
+            nulla, ad et itaque vitae repellat quod aut consequatur deserunt!
+            Similique mollitia dolorum totam? Inventore provident magni odit
+            asperiores temporibus voluptatem animi voluptatum sequi cumque
+            obcaecati, nulla ut vel impedit minus iure.
+          </p>
+          <a href="#8">Заказать</a>
+        </section>
+        <section>
+          <h2>Обо мне</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro unde
+            dolore illum expedita voluptate suscipit cupiditate dolorum,
+            corporis nesciunt fuga optio illo hic, deserunt necessitatibus omnis
+            nihil ipsa quaerat autem quos sint iste totam id. Fuga eos saepe
+            nulla, ad et itaque vitae repellat quod aut consequatur deserunt!
+            Similique mollitia dolorum totam? Inventore provident magni odit
+            asperiores temporibus voluptatem animi voluptatum sequi cumque
+            obcaecati, nulla ut vel impedit minus iure.
+          </p>
+          <img alt="my June Certificate" />
+        </section>
+        <section>
+          <h2>Наши Услуги</h2>
+          <ul>
+            <li>
+              Спсисок вопоросв и требований к разработчику JS уровня June(эти
+              требования предъявлялись ко мне и моим друзьям)
+            </li>
+            <li>Систематизирование шпоргалки</li>
+            <li>Тестовое собеседование со мной 1,5 часа</li>
+            <li>
+              Список типовых задач с осбеседований с решением и объяснением
+            </li>
+            <li>2-е записи реального собеса и один внутрений собес реакт</li>
+            <li>
+              Услуга на проводе, во время собеса я тоже буду на проводе и в
+              двоём мы затащим ваш собес
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2>Отзывы</h2>
+          <p>ddsgfddsfgdg fs gfdssdf ggg sdgf dsdfgfsgdgdg </p>
+        </section>
+      </main>
+      <footer>
+        <ul>
+          <li>vk</li>
+          <li>vk</li>
+          <li>vk</li>
+        </ul>
+      </footer>
     </div>
   );
 }
