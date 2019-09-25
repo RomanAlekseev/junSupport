5ce85980dd570e0ef317a5d190a25c830b1305c3
RomanAlekseev <r.alekseev@andersenlab.com>
2019-09-25 18:53:57 +0300

    add less and bootstrap

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -434,6 +434,19 @@ module.exports = function(webpackEnv) {
               // See https://github.com/webpack/webpack/issues/6571
               sideEffects: true,
             },
+            {
+              test: /\.less$/,
+              exclude: cssModuleRegex,
+              use: getStyleLoaders({
+                importLoaders: 1,
+                sourceMap: isEnvProduction && shouldUseSourceMap,
+              }, "less-loader"),
+              // Don't consider CSS imports dead code even if the
+              // containing package claims to have no side effects.
+              // Remove this when webpack adds a warning or an error for this.
+              // See https://github.com/webpack/webpack/issues/6571
+              sideEffects: true,
+            },
             // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
             // using the extension .module.css
             {
